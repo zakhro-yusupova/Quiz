b2e7ce6f96ea941e8def05640e4a8f3f0dcbd452
zakhro-yusupova <yu.zakhro01@gmail.com>
2022-02-19 04:28:59 +0500

    Add new questions to js file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var name = prompt("Ismingizni kiriting");
 var firstQuestion = prompt("Bootstrap ixtirochisi kim?"); // Mark Otto
-var secondQuestion = prompt("Which class provides a responsive fixed width container?"); // .container
+var secondQuestion = prompt("Bootstrap 5da 1rem nechchi pxga teng?"); // .container
 var thirdQuestion = prompt("Ochiq-ko'k text yaratish uchun qaysi kontekstli class ishlatiladi?"); // .text-info
-var fourthQuestion = prompt("Bootstrap grid tizimi nechta ustunga asoslangan?"); // 12 columns
-var fifthQuestion = prompt("H3 Bootstrap Sarlavhasining standart oʻlchami nechchi px?"); // 24px
-var sixQuestion = prompt("qaysi class qo'shish orqali thumbnail image yaratish mumkin?"); // img-thumbnail
-var seventhQuestion = prompt("alert messageni yopish uchun qaysi class ishlatiladi"); // .alert-dismissible
-var eightQuestion = prompt("qora matn uchun 50% shaffoflikni qo'shadigan class nomini yozing"); // .text-black-50
-var ninthQuestion = prompt("matnni sariqqa bo'yab takidlanganini bildiradigan class qaysi?"); // .text-black-50
-var tenthQuestion = prompt("qaysi class yordamida badge atrofini yanayam dumaloq qilsa bo'ladi?"); // .text-black-50
+var fourthQuestion = prompt("Bootstrapda row ichida nechtagacha ustun joylab bo'ladi?"); // 12 columns
+var fifthQuestion = prompt("Bootstrapni veb-saytga qo'shishning ikkita asosiy usuli qanday??"); // CDN yoli lokal
+var sixQuestion = prompt("Bootstrapda 3 ta teng ustun yaratish uchun qaysi classdan foydalanish kerak?"); // col-4
+var seventhQuestion = prompt("buttonga qaysi class qo'shish orqali buttonni bosib bo'lmaydigan qilsa bo'ladi? "); // .disabled
+var eightQuestion = prompt("lg classi qaysi qurilmalar uchun ishlatiladi"); // .
+var ninthQuestion = prompt("bootstrapda sahifa yaratish uchun qaysi classdan foydalanish kerak?"); // .pagination
+var tenthQuestion = prompt("bootstrap bepul kutubxonami? (true-false)"); // .true
 
 
 console.log(firstQuestion + "\n" + secondQuestion + "\n" + thirdQuestion + "\n" + fourthQuestion + "\n" + fifthQuestion + "\n" + sixQuestion + "\n" + seventhQuestion + "\n" + eightQuestion  + "\n" + ninthQuestion  + "\n" + tenthQuestion);
