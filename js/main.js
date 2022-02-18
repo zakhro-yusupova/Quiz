461461a014332faab0d2d10d042aa4b1f1f6b6ec
zakhro-yusupova <yu.zakhro01@gmail.com>
2022-02-19 03:03:07 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var name = prompt("Ismingizni kiriting");
+var firstQuestion = prompt("Bootstrap ixtirochisi kim?"); // Mark Otto
+var secondQuestion = prompt("Which class provides a responsive fixed width container?"); // .container
+var thirdQuestion = prompt("Ochiq-ko'k text yaratish uchun qaysi kontekstli class ishlatiladi?"); // .text-info
+var fourthQuestion = prompt("Bootstrap grid tizimi nechta ustunga asoslangan?"); // 12 columns
+var fifthQuestion = prompt("H3 Bootstrap Sarlavhasining standart oʻlchami nechchi px?"); // 24px
+var sixQuestion = prompt("qaysi class qo'shish orqali thumbnail image yaratish mumkin?"); // img-thumbnail
+var seventhQuestion = prompt("alert messageni yopish uchun qaysi class ishlatiladi"); // .alert-dismissible
+var eightQuestion = prompt("qora matn uchun 50% shaffoflikni qo'shadigan class nomini yozing"); // .text-black-50
+var ninthQuestion = prompt("matnni sariqqa bo'yab takidlanganini bildiradigan class qaysi?"); // .text-black-50
+var tenthQuestion = prompt("qaysi class yordamida badge atrofini yanayam dumaloq qilsa bo'ladi?"); // .text-black-50
+
+
+console.log(firstQuestion + "\n" + secondQuestion + "\n" + thirdQuestion + "\n" + fourthQuestion + "\n" + fifthQuestion + "\n" + sixQuestion + "\n" + seventhQuestion + "\n" + eightQuestion  + "\n" + ninthQuestion  + "\n" + tenthQuestion);
